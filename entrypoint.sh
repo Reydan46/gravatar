59b0632b8a7137a58d7559e0652c659b8af3257e
Reydan46 <4998320+Reydan46@users.noreply.github.com>
2025-10-11 01:20:51 +0300

    chore(docker): Динамически определять IP шлюза для доверенных прокси
    - Добавлена логика в `entrypoint.sh` для автоматического определения IP-адреса шлюза Docker.
    - Обнаруженный IP-адрес добавляется в переменную окружения `TRUSTED_PROXY_IPS`.
    - Это позволяет приложению корректно определять IP-адрес клиента при работе за прокси-сервером Docker.

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,6 +8,28 @@ if [ "$1" = "hash_password" ]; then
     exit 0
 fi
 
+# ==============================================================================
+# Динамическое определение IP-адреса шлюза Docker для автоматического
+# добавления в доверенные прокси.
+# ==============================================================================
+DOCKER_GATEWAY_IP=$(ip route | awk '/default/ {print $3}')
+
+if [ -n "$DOCKER_GATEWAY_IP" ]; then
+    echo "Docker gateway IP detected: ${DOCKER_GATEWAY_IP}"
+
+    if [ -n "$TRUSTED_PROXY_IPS" ]; then
+        export TRUSTED_PROXY_IPS="${TRUSTED_PROXY_IPS},${DOCKER_GATEWAY_IP}"
+    else
+        export TRUSTED_PROXY_IPS="${DOCKER_GATEWAY_IP}"
+    fi
+else
+    echo "Warning: Could not detect Docker gateway IP."
+fi
+
+
+# ==============================================================================
+# Конфигурация Nginx
+# ==============================================================================
 SSL_CONF_FOUND=
 if ls /etc/nginx/conf.d/*ssl*.conf 1>/dev/null 2>&1; then
     SSL_CONF_FOUND=" ssl"
@@ -21,6 +43,9 @@ sed -i "s/NGINX_PORT/$NGINX_PORT$SSL_CONF_FOUND/g; s/APP_PORT/$APP_PORT/g" /etc/
 echo "----------------------------------------------------"
 echo "Nginx is configured to listen on port: ${NGINX_PORT} (${PROTOCOL})"
 echo "Uvicorn will be started on port:       ${APP_PORT}"
+if [ -n "$TRUSTED_PROXY_IPS" ]; then
+    echo "Trusted proxy IPs set to:              ${TRUSTED_PROXY_IPS}"
+fi
 echo "----------------------------------------------------"
 echo "Starting supervisor..."
 
