54e408edff22197c2be1433e8708b0fc6b8cda36
Reydan46 <4998320+Reydan46@users.noreply.github.com>
2025-10-13 00:50:25 +0300

    chore(config): Изменить порт по умолчанию на 9999
    - Изменить порт приложения по умолчанию с 8000 на 9999.
    - Добавить директиву shellcheck для игнорирования предупреждения SC2086.

--- start-uvicorn.sh
+++ start-uvicorn.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 set -e
 
-UVICORN_ARGS="--host ${APP_HOST:-0.0.0.0} --port ${APP_PORT:-8000} --workers ${APP_WORKERS:-1} --no-access-log --loop uvloop --lifespan on"
+UVICORN_ARGS="--host ${APP_HOST:-0.0.0.0} --port ${APP_PORT:-9999} --workers ${APP_WORKERS:-1} --no-access-log --loop uvloop --lifespan on"
 
 if [ "$APP_RELOAD" = "True" ]; then
     UVICORN_ARGS="$UVICORN_ARGS --reload"
 fi
 
+# shellcheck disable=SC2086
 exec uvicorn main:app $UVICORN_ARGS
