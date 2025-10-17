3891a506bd3319b1988486313d160a702d5f9ed9
reydan46 <4998320+Reydan46@users.noreply.github.com>
2025-10-17 08:42:43 +0300

    style(app): Упорядочить импорты и регистрацию middleware/routers
    - Отсортировать импорты в алфавитном порядке для лучшей читаемости.
    - Сгруппировать и упорядочить вызовы `app.add_middleware`.
    - Сгруппировать и упорядочить вызовы `app.include_router`.

--- app/main.py
+++ app/main.py
@@ -1,7 +1,6 @@
 import logging
 
 from config.constants import LOG_CONFIG, SERVICE_NAME
-from config.settings import settings
 from utils.logger_setup import setup_logging
 
 logger = setup_logging(LOG_CONFIG["main_logger_name"])
@@ -9,19 +8,20 @@ logger = setup_logging(LOG_CONFIG["main_logger_name"])
 from fastapi import FastAPI
 from starlette.staticfiles import StaticFiles
 
+from config.settings import settings
 from api.home.home_router import home_router
 from api.auth.auth_router import auth_router
-from api.avatar.avatar_router import avatar_router
-from api.conf.conf_router import conf_router
 from api.crypto.crypto_router import crypto_router
-from api.gallery.gallery_router import gallery_router
+from api.conf.conf_router import conf_router
 from api.health.health_router import health_router
-from api.ldap.ldap_router import ldap_router
 from api.logs.logs_router import logs_router
+from api.avatar.avatar_router import avatar_router
+from api.gallery.gallery_router import gallery_router
+from api.ldap.ldap_router import ldap_router
 from api.saml.saml_router import saml_router
-from middleware.cors_logging_middleware import LoggingCORSMiddleware
 from middleware.logger_middleware import RequestLoggingMiddleware
 from middleware.protect_middleware import HostAllowMiddleware
+from middleware.cors_logging_middleware import LoggingCORSMiddleware
 from middleware.proxy_middleware import ProxyMiddleware
 from utils.lifespan_utils import lifespan
 from utils.session_context import SessionIdFilter
@@ -43,7 +43,9 @@ app = FastAPI(
 )
 
 
-
+app.add_middleware(RequestLoggingMiddleware)
+# noinspection PyTypeChecker
+app.add_middleware(HostAllowMiddleware)
 app.add_middleware(
     LoggingCORSMiddleware,
     allow_origins=settings.cors_allow_origins,
@@ -51,20 +53,17 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-# noinspection PyTypeChecker
-app.add_middleware(HostAllowMiddleware)
-app.add_middleware(RequestLoggingMiddleware)
 app.add_middleware(ProxyMiddleware)
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 app.include_router(home_router)
 app.include_router(auth_router)
-app.include_router(avatar_router)
-app.include_router(conf_router)
 app.include_router(crypto_router)
-app.include_router(gallery_router)
+app.include_router(conf_router)
 app.include_router(health_router)
-app.include_router(ldap_router)
 app.include_router(logs_router)
+app.include_router(avatar_router)
+app.include_router(gallery_router)
+app.include_router(ldap_router)
 app.include_router(saml_router)
